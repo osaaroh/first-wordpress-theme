09a1aac6f9e2e00ea08e1dffc290fa1e556643d8
“Osaro <saro.iyoha@gmail.com>
2025-09-16 14:05:55 +0100

    Add header.php to root of theme directory

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html <?php language_attributes() ?> >
+    <head>
+        <meta charset="<?php bloginfo('charset')?>">
+        <title><?php bloginfo('name') ?></title>
+        <?php wp_head(); ?>
+    </head>
+    <body>
+        <header>
+            <h1><?php bloginfo('name'); ?></h1>
+            <?php
+            // We want our menu to show here, from functions.php
+                wp_nav_menu(array(
+                    'theme_location' => 'primary-menu'
+                ))
+            ?>
+        </header>
+    </body>
+
+</html>
